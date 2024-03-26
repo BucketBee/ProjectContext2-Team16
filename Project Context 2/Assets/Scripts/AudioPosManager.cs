aa5c5b48d9862b06d100aafd938f64c410e05d07
TienVanTheTen <e.c.games@outlook.com>
2024-03-26 21:46:52 +0100

    audioposmanager

---
+++ Project Context 2/Assets/Scripts/AudioPosManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioPosManager : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _transformCam;
+    [SerializeField]
+    private Transform _transformCamRig;
+
+    private float posX;
+    private float posY;
+    private float posZ;
+ 
+    // Update is called once per frame
+    void Update()
+    {
+        posX = _transformCamRig.position.x;
+        posZ = _transformCamRig.position.z;
+
+        posY = _transformCam.position.y;
+
+        transform.position = new Vector3(posX,posY,posZ);
+    }
+}
