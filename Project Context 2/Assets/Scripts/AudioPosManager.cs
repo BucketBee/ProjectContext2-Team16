fbd8fbbfadf1f7ca2d4e0e5044a5275ffed2f1d0
TienVanTheTen <e.c.games@outlook.com>
2024-03-27 21:24:56 +0100

    struggles

--- Project Context 2/Assets/Scripts/AudioPosManager.cs
+++ Project Context 2/Assets/Scripts/AudioPosManager.cs
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -25,31 +25,4 @@ public class AudioPosManager : MonoBehaviour
         transform.position = new Vector3(posX,posY,posZ);
     }
 }
-=======
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 
-public class AudioPosManager : MonoBehaviour
-{
-    [SerializeField]
-    private Transform _transformCam;
-    [SerializeField]
-    private Transform _transformCamRig;
-
-    private float posX;
-    private float posY;
-    private float posZ;
- 
-    // Update is called once per frame
-    void Update()
-    {
-        posX = _transformCamRig.position.x;
-        posZ = _transformCamRig.position.z;
-
-        posY = _transformCam.position.y;
-
-        transform.position = new Vector3(posX,posY,posZ);
-    }
-}
->>>>>>> AudioReformedYes
